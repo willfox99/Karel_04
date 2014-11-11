3b2e13d9d638329a0ab25056e0b36c6c8fd5f8cd
Kent Collins <kent.r.collins@gmail.com>
2014-11-11 20:40:54 +0000

    updated

--- Lab04.java
+++ Lab04.java
@@ -1,6 +1,6 @@
 /**
 *
-* Description of the program goes here  // provide a brief description
+* Creates a group of Athletes and directs them to positions on the field
 *
 * @author <Your Name here>  // replace <...> with your name
 * @version <date/of/completion> // replace <...> with the date
@@ -10,6 +10,16 @@ import edu.fcps.karel2.Display;
 import edu.fcps.karel2.Robot;
 
 public class Lab04 {
+	
+	public static void takeTheField(Athlete arg) {
+		arg.move();
+		arg.move();
+		arg.move();
+		arg.move();
+		arg.turnRight();
+		arg.move();
+		arg.move();
+	}
 
      public static void main(String[] args) {
           // TODO Your code goes here
