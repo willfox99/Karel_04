8ab955c309e57f73089abf842cab7b931dfc9ac4
willfox99 <william_fox@asl.org>
2014-11-20 14:00:11 +0000

    Finished

--- Lab04.java
+++ Lab04.java
@@ -1,88 +1,77 @@
-/**
-*
-* Creates a group of Athletes and directs them to positions on the field
-*
-* @author Will Fox
-* @version 2/11/2014
-*/
+/*
+  * @author Will Fox
+  * @version 31/10/2014
+ */
+ 
+ import edu.fcps.karel2.Display;
+ 
+ 
+ public class Lab04 {
+ 
 
-import edu.fcps.karel2.Display;
-import edu.fcps.karel2.Robot;
+       public static void takeTheField(Athlete arg) {
+            arg.move();
+            arg.move();
+            arg.move();
+            arg.move();
+            arg.turnRight();
+            arg.move();
+            arg.move();
+      }
 
-public class Lab04 {
+        public static void main (String[] args) {
+        
+              Display.openWorld("maps/arena.map");
+              Display.setSize(10, 10);
+              Display.setSpeed(9);
+              
+        
+        Athlete a = new Athlete();
+        Athlete b = new Athlete();
+        Athlete c = new Athlete();
+        Athlete d = new Athlete();
+        Athlete e = new Athlete();
+        Athlete f = new Athlete();
+        
+        takeTheField(a);
 	
-	public static void takeTheField(Athlete arg) {
-		arg.move();
-		arg.move();
-		arg.move();
-		arg.move();
-		arg.turnRight();
-		arg.move();
-		arg.move();
-	}
-
-     public static void takeTheField(Athlete arg) {
-          arg.move();
-          arg.move();
-          arg.move();
-          arg.move();
-          arg.turnRight();
-          arg.move();
-          arg.move();
-     }
-
-   public static void main (String[] args) {
-            Display.openWorld("maps/arena.map");
-            Display.setSize(10, 10);
-            Display.setSpeed(9);
-            
-      Robot coach = new Robot(1, 7, Display.EAST, 0);
-      Athlete a = new Athlete();
-      Athlete b = new Athlete();
-      Athlete c = new Athlete();
-      Athlete d = new Athlete();
-      Athlete e = new Athlete();
-      Athlete f = new Athlete();
-      
-      takeTheField(a);
-
-      takeTheField(b);
-      takeTheField(c);
-      takeTheField(d);
-      takeTheField(e);
-      takeTheField(f);
-      
-      a.move();
-      a.move();
-      a.move();
-      a.turnLeft();
-      a.move();
-      a.move();
-      a.turnAround();
-      b.move();
-      b.turnLeft();
-      b.move();
-      b.turnAround();
-      c.move();
-      c.move();
-      c.turnRight();
-      d.move();
-      d.move();
-      d.move();
-      d.turnRight();
-      e.move();
-      e.move();
-      e.move();
-      e.move();
-      e.turnRight();
-      f.move();
-      f.move();
-      f.move();
-      f.move();
-      f.move();
-      f.turnLeft();
-      f.move();
-      f.turnAround();
-      
-     }
-}
+        takeTheField(b);
+        takeTheField(c);
+        takeTheField(d);
+        takeTheField(e);
+        takeTheField(f);
+        
+        a.move();
+        a.move();
+        a.move();
+        a.turnLeft();
+        a.move();
+        a.move();
+        a.turnAround();
+        b.move();
+        b.turnLeft();
+        b.move();
+        b.turnAround();
+        c.move();
+        c.move();
+        c.turnRight();
+        d.move();
+        d.move();
+        d.move();
+        d.turnRight();
+        e.move();
+        e.move();
+        e.move();
+        e.move();
+        e.turnRight();
+        f.move();
+        f.move();
+        f.move();
+        f.move();
+        f.move();
+        f.turnLeft();
+        f.move();
+        f.turnAround();
+        
+       }
+  }
